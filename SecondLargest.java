414ad84127d530b27a9d97934de490599723f5c2
Arpan Mondal <155351062+a123r123p@users.noreply.github.com>
2026-02-20 12:47:56 +0530

    Update Java solution to preferred Solution class format

--- SecondLargest.java
+++ SecondLargest.java
@@ -1,35 +1,49 @@
 import java.util.Scanner;
 
-public class SecondLargest {
-    public static int secondLargest(int[] arr) {
-        int largest = -1;
-        int second = -1;
-
-        for (int value : arr) {
-            if (value > largest) {
-                second = largest;
-                largest = value;
-            } else if (value < largest && value > second) {
-                second = value;
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        if (arr.length < 2) {
+            return -1;
+        }
+        int max1, max2;
+        if (arr[0] > arr[1]) {
+            max1 = arr[0];
+            max2 = arr[1];
+        } else if (arr[0] < arr[1]) {
+            max1 = arr[1];
+            max2 = arr[0];
+        } else {
+            max1 = arr[0];
+            max2 = Integer.MIN_VALUE;
+        }
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] > max1) {
+                max2 = max1;
+                max1 = arr[i];
+            } else if (arr[i] > max2 && arr[i] != max1) {
+                max2 = arr[i];
             }
         }
-
-        return second;
+        if (max2 == Integer.MIN_VALUE) {
+            return -1;
+        }
+        return max2;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter number of elements: ");
+        System.out.println("Enter number of elements: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
-        System.out.print("Enter elements separated by space: ");
+        System.out.println("Enter elements seperated by space: ");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
-        System.out.println(secondLargest(arr));
+        Solution sol = new Solution();
+        System.out.println(sol.getSecondLargest(arr));
         sc.close();
     }
 }
